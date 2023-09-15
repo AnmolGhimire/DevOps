dc9d14cb41c049f7733622a5596e1faf2fa1b5bd
anmol <anmol@gmail.com>
2023-09-15 10:45:49 +0000

    added build and deploy

---
+++ docker/docker-examples/hello.sh
@@ -0,0 +1,8 @@
+cd docker/docker-examples
+echo "#################################"
+whoami
+echo "##################################"
+docker image build -t docanmol/morning-devops:v$BUILD_NUMBER .
+docker push docanmol/morning-devops:v$BUILD_NUMBER
+docker container  run -itd --name myapp -p 8090:3000 docanmol/morning-devops:v$BUILD_NUMBER
+docker container ps

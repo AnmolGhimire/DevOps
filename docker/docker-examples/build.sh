6d4104399117ffc465cabcf74bcf99a4518e70fc
anmol <anmol@gmail.com>
2023-09-15 10:56:36 +0000

    name chnage

--- docker/docker-examples/build.sh
+++ docker/docker-examples/build.sh
@@ -4,14 +4,14 @@ echo "##########"
 whoami
 echo "##########"
 echo "Building image"
-docker image build -t suryaraj/morning-dev:v$BUILD_NUMBER .
+docker image build -t docanmol/morning-devops:v$BUILD_NUMBER .
 echo "Push image to docker registry"
-docker push suryaraj/morning-dev:v$BUILD_NUMBER
+docker push docanmol/morning-devops:v$BUILD_NUMBER
 echo "Stopping existing container"
 docker container stop myapp || true
 echo "Removing container"
 docker container rm myapp || true
 echo "Creating new container with new build"
-docker container run -itd --name myapp -p 8090:3000 suryaraj/morning-dev:v$BUILD_NUMBER
+docker container run -itd --name myapp -p 8090:3000 docanmol/morning-devops:v$BUILD_NUMBER
 echo "Listing running containers"
 docker container ps
